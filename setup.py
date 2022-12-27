d0b849e7fe968bbf435c9cb60949bf9db5aac8ff
Jourdelune <jourdelune863@gmail.com>
2022-12-27 14:10:43 +0100

    add readme to pip

--- setup.py
+++ setup.py
@@ -10,13 +10,14 @@ setup(
     url='https://github.com/Interaction-Bot/LanguageDetection/',
     author='Jourdelune',
     license='MIT',
-    version='0.0.4',
+    version='0.0.5',
     long_description=readme(),
+    long_description_content_type='text/markdown',
     install_requires=[
         'fasttext',
         'emoji',
         'lingua-language-detector'
     ],
-    keywords = ["language", "detection", "nlp", "identification"],
+    keywords = ["language", "detection", "nlp", "identification", "short"],
     include_package_data=True,
 )
